2c8788d1c6d90aa698a7469b163821196b8a37f0
V-Icaro <venicius.icarosr@gmail.com>
2020-04-29 16:16:38 -0300

    Definido rotas e navegação

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './login.css';
+import { Link } from 'react-router-dom';
 
 import firebase from '../../config/firebase';
 import 'firebase/auth';
@@ -45,7 +46,7 @@ function Login(){
             <div className="opcoes-login mt-5">
                 <a href="#" className="mx-2">Recuperar Senha</a>
                 <span className="text-white">&#9733;</span>
-                <a href="#" className="mx-2">Quero cadastrar</a>
+                <Link to="novousuario" className="mx-2">Quero cadastrar</Link>
             </div>
             
 
