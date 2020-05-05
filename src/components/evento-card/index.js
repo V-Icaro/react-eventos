ca764de9cdd863c1598327502f358810899d2fca
V-Icaro <venicius.icarosr@gmail.com>
2020-05-04 22:13:12 -0300

    Home listando eventos buscando no firebase

--- src/components/evento-card/index.js
+++ src/components/evento-card/index.js
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
+import firebase from '../../config/firebase';
 
-function EventoCard(){
+function EventoCard({key, id, img, titulo, detalhes, visualizacoes}){
+
+    const [urlImg, setUrlImg] = useState();
+
+    useEffect(() => {
+        firebase.storage().ref(`imagens/${img}`).getDownloadURL().then(url => setUrlImg(url));
+    })
+
     return(
         <div className="col-md-3 col-sm-12">
-            <img src="https://via.placeholder.com/50" className="card-img-top img-cartao" alt="imagem do evento"/>
+            <img src={urlImg} className="card-img-top img-cartao" alt="imagem do evento"/>
 
             <div className="card-body">
-                <h5>Titulo do Evento</h5>
-                <p className="card-text test-justify">Detalhes do Evento</p>
+                <h5>{titulo}</h5>
+                <p className="card-text test-justify">{detalhes}</p>
 
                 <div className="row rodape-card d-flex align0items-center">
                     <div className="col-6">    
-                        <Link to="/" className="btn btn-sm btn-detalhes">+ detalhes</Link>
+                        <Link to={"/eventodetalhes/" + id} className="btn btn-sm btn-detalhes">+ detalhes</Link>
                     </div>
 
                     <div className="col-6 text-right">
-                        <i class="fas fa-eye"></i><span> 2019</span>
+                        <i class="fas fa-eye"></i> <span>{visualizacoes}</span>
                     </div>
                 </div>
             </div>
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,24 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+/* --COMPONENTS-- */ 
 import Navbar from '../../components/navbar/';
 import EventoCard from '../../components/evento-card';
 
+import firebase from '../../config/firebase';
+
 function Home(){
+
+    const [eventos, setEventos] = useState([]);
+    let listaeventos = [];
+
+    useEffect(() => {
+        firebase.firestore().collection('eventos').get()
+            .then(async (resultado) => {
+                await resultado.docs.forEach(doc => {
+                    listaeventos.push({
+                        id: doc.id,
+                        ...doc.data()
+                    });
+                });
+
+                setEventos(listaeventos);
+            });
+    });
+
     return (
         <>
-        <Navbar />
-        <h1>{useSelector(state => state.usuarioEmail)}</h1>
-        <h1>Logado: {useSelector(state => state.usuarioLogado)}</h1>
+        <Navbar /> 
 
-        <div className="row">
-            <EventoCard />
-            <EventoCard />
-            <EventoCard />
-            <EventoCard />
-            <EventoCard />
+        <div className="row p-3">
+        {eventos.map(item => <EventoCard key={item.id} id={item.id} img={item.foto} titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes}/>)}
+            
         </div>
 
         </>
